249c9d8d26f6c6de6f524c74a2fb31024cbac505
guy1397 <guy.guppy@gmail.com>
2021-05-23 11:01:19 +0100

    initial commit

---
+++ Scripts/main.js
@@ -0,0 +1,11 @@
+let sport;
+let fat;
+
+// Select the appropriate calculator
+function calculator() {
+  console.log('calculating');
+}
+
+// Run function
+// const variable is to capture value from function
+const calculatorResult = calculator();
